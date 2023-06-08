cf139519721877a2583bcea958560da0e2dd1264
tanamaru <yt.0408.mmm@gmail.com>
2023-06-08 22:51:51 +0900

    自動的に現在地を取得

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get '/home', to: 'home#index'
   resources :maps
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
